df0cf6173da94eb47f589779fee2ac71f61e1958
Matthew Deloughry <matt@deloughry.co.uk>
2021-03-04 08:51:12 +0000

    :wrench: update user list

--- index.ts
+++ index.ts
@@ -49,33 +49,101 @@ async function getSchedule(user: string) {
 
 
 const users = [
-  "brattdamon",
-  "cadillacjack1",
-  "canhorn",
-  "codejuration",
-  "lucecarter",
-  "contentfuldevs",
-  "cowboy_salmon",
-  "dr_dinomight",
-  "exegeteio",
-  "gacbl",
-  "jwalter",
-  "laylacodesit",
-  "matty_twoshoes",
-  "rawwwrs",
-  "ryantupo",
-  "ryan_the_rhg",
-  "sadmoody",
-  "sketchni",
-  "sociablesteve",
-  "thatn00b__",
-  "toefrog",
-  "greg_holmes",
-  "madhouseminers",
-  "whitep4nth3r",
+  {
+    name: "baldbeardedbuilder",
+    id: "279965339",
+  },
+  {
+    name: "brattdamon",
+    id: "254737658",
+  },
+  {
+    name: "cadillacjack1",
+    id: "501793804",
+  },
+  {
+    name: "canhorn",
+    id: "60218113",
+  },
+  {
+    name: "codejuration",
+    id: "25116716",
+  },
+  {
+    name: "lucecarter",
+    id: "199566394",
+  },
+  {
+    name: "contentfuldevs",
+    id: "576507866",
+  },
+  {
+    name: "dr_dinomight",
+    id: "25347823",
+  },
+  {
+    name: "exegeteio",
+    id: "40856022",
+  },
+  {
+    name: "gacbl",
+    id: "120572949",
+  },
+  {
+    name: "jwalter",
+    id: "50911906",
+  },
+  {
+    name: "laylacodesit",
+    id: "260151116",
+  },
+  {
+    name: "matty_twoshoes",
+    id: "556670211",
+  },
+  {
+    name: "rawwwrs",
+    id: "166942660",
+  },
+  {
+    name: "ryantupo",
+    id: "158165150",
+  },
+  {
+    name: "ryan_the_rhg",
+    id: "154364425",
+  },
+  {
+    name: "sadmoody",
+    id: "28493092",
+  },
+  {
+    name: "sketchni",
+    id: "64115778",
+  },
+  {
+    name: "sociablesteve",
+    id: "76884091",
+  },
+  {
+    name: "thatn00b__",
+    id: "235952406",
+  },
+  {
+    name: "toefrog",
+    id: "50336378",
+  },
+  {
+    name: "greg_holmes",
+    id: "93948214",
+  },
+  {
+    name: "aurorasmadhouse",
+    id: "132113595",
+  },
 ];
 
-Promise.all(users.map((u) => getSchedule(u))).then(() => {
+Promise.all(users.map((u) => getSchedule(u.name))).then(() => {
   fs.writeFile('./public/scripts/output.js', 'var schedule = ' + JSON.stringify(schedule),  function(err) {
       if (err) {
           return console.error(err);
